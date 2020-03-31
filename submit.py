5d13be51cd7a3a228c5668b282567fd4ee514aa4
Soowin <33149289+Soowin@users.noreply.github.com>
2020-03-31 22:29:00 +0800

    basic changes

--- submit.py
+++ submit.py
@@ -1,10 +1,14 @@
 # -*- coding:UTF-8 -*-
 
-import requests, json, re, sys
-import re
-from PIL import Image
 import io
+import json
+import re
+import sys
 from urllib import parse
+
+import requests
+from PIL import Image
+
 
 def main():
     rawdata = {}
@@ -36,38 +40,41 @@ def zfw():
     rawdata = {}
     with open("rawdata.json","r") as rd:
         rawdata = json.load(rd)
-    
+    useHeader = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36"
+    }
     # Connect
     conn = requests.Session()
-    use_headers = {
-        "Content-Type": "text/html; charset = UTF-8",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
-        "Referer": "https://zfw.xidian.edu.cn/"
-    }
-    pageres = conn.get("https://zfw.xidian.edu.cn",headers = use_headers)
+    pageres = conn.get("https://zfw.xidian.edu.cn/login", headers = useHeader)
     # Get Verify code
-    csrfdata = re.search('<meta name="csrf-token" content="(.*)">',pageres.text).group(1)
-    verifyimgURL = "https://zfw.xidian.edu.cn" + re.search('<img id="loginform-verifycode-image" src="(.*)" alt="">',pageres.text).group(1)
-    verify_code = showimage(verifyimgURL)
+    #csrfdata = re.search('<meta name="csrf-token" content="(.*)">',pageres.text).group(1)
+    csrfdata = re.search('<input type="hidden" name="_csrf" value="(.*)">		<div class="header">',pageres.text).group(1)
+    #verifyimgURL = "https://zfw.xidian.edu.cn" + re.search('<img id="loginform-verifycode-image" src="(.*)" alt="">',pageres.text).group(1)
+    #verify_code = showimage(verifyimgURL)
     # Post fromdata
     # result = conn.post("https://zfw.xidian.edu.cn",data={"_csrf":csrfdata,"LoginForm[username]":rawdata["userID"],"LoginForm[password]":rawdata["userPSWD"],"LoginForm[verifyCode]":verify_code,"login-button":NULL})
-    # formdata = parse.quote("_csrf="+csrfdata+"&LoginForm[username]="+rawdata["userID"]+"&LoginForm[password]="+rawdata["userPSWD"]+"&LoginForm[verifyCode]"+verify_code+"&login-button=")
+    #formdata = parse.quote("_csrf="+csrfdata+"&LoginForm[username]="+rawdata["userID"]+"&LoginForm[password]="+rawdata["userPSWD"]+"&LoginForm[verifyCode]"+verify_code+"&login-button=")
+    #formdata = "_csrf="+ parse.quote(csrfdata) + "&LoginForm%5Busername%5D="+rawdata["userID"]+"&LoginForm%5Bpassword%5D="+rawdata["userPSWD"]+"&LoginForm%5BverifyCode%5D="+verify_code+"&login-button="
     formdata = {
         "_csrf":csrfdata,
         "LoginForm[username]":rawdata["userID"],
         "LoginForm[password]":rawdata["userPSWD"],
-        "LoginForm[verifyCode]":verify_code,
-        "login-button":""
+        # "LoginForm[verifyCode]":verify_code,
+        # "login-button":""
         }
     
     # Return login result
-    result = conn.post("https://zfw.xidian.edu.cn", data=formdata)
+    result = conn.post("https://zfw.xidian.edu.cn/login", data=formdata, headers = useHeader)
     try:
-        dataLeft = re.search('<td data-col-seq="7">(.*)</td>',result.text).group(1)
+        pattern = re.compile(r'([0-9]+.?[0-9]*(byte|GB|MB))')
+        dataLeft = pattern.findall(result.text)
     except AttributeError:
         print("Login filed!")
     else:
-        print("剩余流量: " + dataLeft)
+        print("每月免费10GB流量，已使用: " + str(dataLeft[0][0]))
+        print("每月免费10GB流量，剩余: " + str(dataLeft[1][0]))
+        print("充值流量已使用: " + str(dataLeft[2][0]))
+        print("充值流量剩余: " + str(dataLeft[3][0]))
     
     conn.close()
 
