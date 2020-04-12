db7c2ee5deda8a50808f54edc67aecfd7c2d63cc
Soowin <33149289+Soowin@users.noreply.github.com>
2020-04-12 20:40:39 +0800

    PyCharm Format

--- Learnjson.py
+++ Learnjson.py
@@ -2,6 +2,7 @@
 
 import json
 
+
 def AskBoolean(tips):
     ans = input(tips + "<Y/N> ").upper()
     if ans == "Y":
@@ -9,20 +10,21 @@ def AskBoolean(tips):
     if ans == "N":
         return 0
 
-rawdata={}
 
-rawdata.update({"userID":input("Enter your ID: ")})
-rawdata.update({"userPSWD":input("Enter your Password: ")})
-# rawdata.update({"sfzx":AskBoolean("Have you been in Hubei today?")})
+if __name__ == '__main__':
+    rawdata = {}
 
-print("Your info is as followed:")
-print(rawdata)
+    rawdata.update({"userID": input("Enter your ID: ")})
+    rawdata.update({"userPSWD": input("Enter your Password: ")})
+    # rawdata.update({"sfzx":AskBoolean("Have you been in Hubei today?")})
 
-with open("rawdata.json","w") as fileLink:
-    try:
-        json.dump(rawdata,fileLink)
-    except IOError:
-        print("IO error.")
-    else:
-        print("Done!")
+    print("Your info is as followed:")
+    print(rawdata)
 
+    with open("rawdata.json", "w") as fileLink:
+        try:
+            json.dump(rawdata, fileLink)
+        except IOError:
+            print("IO error.")
+        else:
+            print("Done!")
--- submit.py
+++ submit.py
@@ -12,31 +12,31 @@ from PIL import Image
 
 def checkID():
     rawdata = {}
-    with open("rawdata.json","r") as rd:
+    with open("rawdata.json", "r") as rd:
         rawdata = json.load(rd)
-
     conn = requests.Session()
 
     ## Login
-
-    result = conn.post("https://xxcapp.xidian.edu.cn/uc/wap/login/check",data={"username":rawdata["userID"],"password":rawdata["userPSWD"]})
+    result = conn.post("https://xxcapp.xidian.edu.cn/uc/wap/login/check",
+                       data={"username": rawdata["userID"], "password": rawdata["userPSWD"]})
     if result.status_code == 200:
         logindata = {}
         logindata = json.loads(result.text)
         if logindata["m"] == "账号或密码错误":
             print("Login Failed.\nWrong UserID or Password.\n")
             sys.exit()
-        else: pass
+        else:
+            pass
     else:
         print("Internet Connection Failed.")
         sys.exit()
 
-def showimage(URL):
 
+def showimage(URL):
     conn = requests.Session()
     pageres = conn.get(URL)
-    
-    img=Image.open(io.BytesIO(pageres.content))
+
+    img = Image.open(io.BytesIO(pageres.content))
     img.show()
 
     return input("Input the code: ")
@@ -44,41 +44,45 @@ def showimage(URL):
 
 def main():
     rawdata = {}
-    with open("rawdata.json","r") as rd:
+    with open("rawdata.json", "r") as rd:
         rawdata = json.load(rd)
     useHeader = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko)"
+                      " Chrome/80.0.3987.149 Mobile Safari/537.36"
     }
     # Check ID
     checkID()
     # Connect
     conn = requests.Session()
-    pageres = conn.get("https://zfw.xidian.edu.cn/login", headers = useHeader)
+    pageres = conn.get("https://zfw.xidian.edu.cn/login", headers=useHeader)
     # Get Verify code
-    #csrfdata = re.search('<meta name="csrf-token" content="(.*)">',pageres.text).group(1)
-    csrfdata = re.search('<input type="hidden" name="_csrf" value="(.*)">		<div class="header">',pageres.text).group(1)
-    #verifyimgURL = "https://zfw.xidian.edu.cn" + re.search('<img id="loginform-verifycode-image" src="(.*)" alt="">',pageres.text).group(1)
-    #verify_code = showimage(verifyimgURL)
-    # Post fromdata
-    # result = conn.post("https://zfw.xidian.edu.cn",data={"_csrf":csrfdata,"LoginForm[username]":rawdata["userID"],"LoginForm[password]":rawdata["userPSWD"],"LoginForm[verifyCode]":verify_code,"login-button":NULL})
-    #formdata = parse.quote("_csrf="+csrfdata+"&LoginForm[username]="+rawdata["userID"]+"&LoginForm[password]="+rawdata["userPSWD"]+"&LoginForm[verifyCode]"+verify_code+"&login-button=")
-    #formdata = "_csrf="+ parse.quote(csrfdata) + "&LoginForm%5Busername%5D="+rawdata["userID"]+"&LoginForm%5Bpassword%5D="+rawdata["userPSWD"]+"&LoginForm%5BverifyCode%5D="+verify_code+"&login-button="
+    # csrfdata = re.search('<meta name="csrf-token" content="(.*)">',pageres.text).group(1)
+    csrfdata = re.search('<input type="hidden" name="_csrf" value="(.*)">		<div class="header">',
+                         pageres.text).group(1)
+    # verifyimgURL = "https://zfw.xidian.edu.cn" + re.search('<img id="loginform-verifycode-image" src="(.*)"
+    # alt="">',pageres.text).group(1) verify_code = showimage(verifyimgURL) Post fromdata result = conn.post(
+    # "https://zfw.xidian.edu.cn",data={"_csrf":csrfdata,"LoginForm[username]":rawdata["userID"],"LoginForm[
+    # password]":rawdata["userPSWD"],"LoginForm[verifyCode]":verify_code,"login-button":NULL}) formdata =
+    # parse.quote("_csrf="+csrfdata+"&LoginForm[username]="+rawdata["userID"]+"&LoginForm[password]="+rawdata[
+    # "userPSWD"]+"&LoginForm[verifyCode]"+verify_code+"&login-button=") formdata = "_csrf="+ parse.quote(csrfdata) +
+    # "&LoginForm%5Busername%5D="+rawdata["userID"]+"&LoginForm%5Bpassword%5D="+rawdata[
+    # "userPSWD"]+"&LoginForm%5BverifyCode%5D="+verify_code+"&login-button="
     formdata = {
-        "_csrf":csrfdata,
-        "LoginForm[username]":rawdata["userID"],
-        "LoginForm[password]":rawdata["userPSWD"],
+        "_csrf": csrfdata,
+        "LoginForm[username]": rawdata["userID"],
+        "LoginForm[password]": rawdata["userPSWD"],
         # "LoginForm[verifyCode]":verify_code,
         # "login-button":""
-        }
-    
+    }
+
     # Return login result
-    result = conn.post("https://zfw.xidian.edu.cn/login", data=formdata, headers = useHeader)
+    result = conn.post("https://zfw.xidian.edu.cn/login", data=formdata, headers=useHeader)
     try:
         pattern = re.compile(r'([0-9]+.?[0-9]*(byte|GB|MB))')
         dataLeft = pattern.findall(result.text)
-        accountType = re.search(r'<span class="progress-text title-main">(.*)<\/span>',result.text).group(1)
-        username = re.search(r'<label class="list-group-label">姓名<\/label>(.*)<\/li>',result.text).group(1)
-        nextloopdate = re.search(r'<span class="package-value">(.*)<\/span>',result.text).group(1)
+        accountType = re.search(r'<span class="progress-text title-main">(.*)<\/span>', result.text).group(1)
+        username = re.search(r'<label class="list-group-label">姓名<\/label>(.*)<\/li>', result.text).group(1)
+        nextloopdate = re.search(r'<span class="package-value">(.*)<\/span>', result.text).group(1)
     except AttributeError:
         print("Login filed!")
     else:
@@ -89,7 +93,7 @@ def main():
         print("下次免费流量重置日期: " + str(nextloopdate))
         print("充值流量已使用: " + str(dataLeft[2][0]))
         print("\033[32m充值流量剩余: " + str(dataLeft[3][0]) + "\033[0m")
-    
+
     conn.close()
 
 
